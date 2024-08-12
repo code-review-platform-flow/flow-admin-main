1b17a7e9a38dbe6016871f1c46741488f888ae4f
abwarten <a1061602@gmail.com>
2024-08-12 10:43:29 +0900

    Init

---
+++ src/main/java/com/flow/admin/main/FlowAdminMainApplication.java
@@ -0,0 +1,13 @@
+package com.flow.admin.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAdminMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowAdminMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flow/admin/main/FlowAdminMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.admin.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowAdminMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
